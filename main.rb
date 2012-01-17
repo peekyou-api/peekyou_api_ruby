13a069f6cde623842c0604c02af018387f71dd03
ANKIT <yopresent@gmail.com>
2012-01-17 11:49:08 -0500

    Added consumer API support

--- main.rb
+++ main.rb
@@ -3,15 +3,21 @@
 require 'api.rb'
 
 d = Peekyou.new
+#set your key here
 d.set_key("YOUR API KEY GOES HERE")
 #Frequency is optional 
 d.set_frequency(1)
 
+#set your app_id here if you are using consumer audience API
+d.set_consumer("YOUR APP ID GOES HERE")
 # @param url
 # @param type (only JSON or XML)
 # the first parameter is your url and second parameter is the format the output will be returned
 
-puts d.get_url("http://twitter.com/michaesafasdfasdlhussey","xml")
+#calling social audience API
+puts d.social_audience_info("http://twitter.com/michaelhussey234234","json")
+#calling consumer audience API
+puts d.consumer_audience_info("http://twitter.com/joshmackey","xml")
 
 
 
